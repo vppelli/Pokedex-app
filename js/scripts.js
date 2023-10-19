bd8774e7d7169ff614f098c96b89516f4b794b76
Mikey P <mpworkplace@gmail.com>
2023-10-19 13:52:52 -0400

    Updated scripts.js
    Added new variable called clashTroopList
    Added 3 objects (Troops)
    Removed placeholder text

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello World');
-let favoriteFood = 'Hamburger';
-document.write(favoriteFood);
+let clashTroopList = [{ name: "Barbarian", maxLvl: 11, type: ['ragedBarbarian', 'homeBarabarian', 'capitalBarbarian'] }, { name: "Archer", maxLvl: 11, type: ['sneakArcher', 'homeArcher', 'capitalArcher'] }, {name: "Giant", maxLvl: 11, type: ['boxerGiant','homeGiant','capitalGiant']}]
