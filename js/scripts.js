8247086180643220e5998208a8711eaf51ee17cb
Mikey P <mpworkplace@gmail.com>
2023-10-20 16:48:39 -0400

    Updated the scripts.js, index.html. styles.css
    Added a Loop for pokemonList
    Gives
    - Name
    - Height
    - Special text if Large type Pokemon
    
    Changed font size

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-let pokemonList = [{ name: "Bulbasaur", height: 2.04, type: ['grass', 'poison'] }, { name: "Charmander", height: 2, type: ['fire'] }, {name: "Squirtle", height: 1.08, type: ['water']}]
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 2.04,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 2,
+        type: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 1.08,
+        type: ['water']
+    }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 2.02 && pokemonList[1].height > 1.5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    } else if (pokemonList[i].height < 1.5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    } else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow that\'s Big Pokemon!! ');
+    }
+}
