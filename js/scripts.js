d1a9d65cf07f3888a753b9559cc7300ba0569191
Mikey P <mpworkplace@gmail.com>
2023-10-24 13:44:13 -0400

    Updated the scripts.js
    Added IIFE to pokemonList
    Changed for Loops to forEach
    Added 2 returns to IIFE
    - getAll
    - add

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,48 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 2.04,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 2,
-        type: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 1.08,
-        type: ['water']
-    }
-];
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 2) {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow that\'s a Big Pokemon!! ' + '</p>');
-    } else {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 2.04,
+            type: ['Grass', 'Poison']
+        },
+        {
+            name: 'Charmander',
+            height: 2,
+            type: ['Fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 1.08,
+            type: ['Water']
+        }
+    ];
+
+    pokemonList.forEach(function (pokemon) {
+        if (pokemon.height > 2) {
+            document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') ' + 'Type: ' + pokemon.type + ' - WOW That\'s a Big Pokemon! ' + '</p>');
+        }else {
+            document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') ' + 'Type: ' + pokemon.type + '</p>');
+        }
+    });
+
+    function getAll() {
+        return pokemonList;
     }
-}
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({name: 'Pikachu' });
+
+console.log(pokemonRepository.getAll());
